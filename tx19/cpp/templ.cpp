e0388adfcfd5b85ead869d0c4b11528cc8a93956
jqasbcd20z <jqasbcd20z@q.com>
2020-09-05 12:39:26 +0100

    add-static_member.cpp-templ.cpp

---
+++ tx19/cpp/templ.cpp
@@ -0,0 +1,24 @@
+#if 0
+#set -x
+g++ -g   -std=c++11 $0 -o $0.ell ||exit -1
+#./$0.ell
+#g++ -g -Wall  -std=c++11 -fno-elide-constructors  $0 -o $0.ell ||exit -1
+ls -l $0*
+./$0.ell
+exit 0;
+##
+
+#endif
+
+#include <iostream>
+#include <string>
+#include <stdio.h>
+using namespace std;
+
+
+
+int main()
+{
+
+return 0;
+}
