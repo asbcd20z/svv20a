2d51df557e3344eaa8179138b1b8fdbba09182d4
jqasbcd20z <jqasbcd20z@q.com>
2020-12-20 13:57:11 +0000

    descriptor.py--update

--- tx19/python/descriptor.py
+++ tx19/python/descriptor.py
@@ -8,7 +8,29 @@
 #python核心编程v2-13.16.3  特殊方法__getattribute__()
 #
 #Python的__getattribute__二三事 https://www.cnblogs.com/telecomshy/p/10605679.html
-#Python描述符深入理解            https://www.cnblogs.com/telecomshy/p/10168853.html
+#Python描述符深入理解        #   https://www.cnblogs.com/telecomshy/p/10168853.html
+#
+#深入理解 Python 描述符  (why#)  https://www.cnblogs.com/wongbingming/p/13555963.html
+'''
+2. 描述符的访问规则#
+描述符分两种：
+数据描述符：实现了__get__ 和 __set__ 两种方法的描述符
+非数据描述符：只实现了__get__ 一种方法的描述符
+你一定会问，他们有什么区别呢？网上的讲解，我看过几个，很多都把一个简单的东西讲得复杂了。
+--
+其实就一句话，数据描述器和非数据描述器的区别在于：它们相对于实例的字典的优先级不同。
+如果实例字典中有与描述符同名的属性，如果描述符是数据描述符，优先使用数据描述符，如果是非数据描述符，优先使用字典中的属性。
+'''
+#
+#深入理解Python描述符 https://blog.csdn.net/woshijidutu/article/details/77893318?utm_source=blogxgwz6
+# 《流畅的Python》  根据描述符中是否定义了__set__方法可以把描述符分为覆盖型和非覆盖型，有__set__方法就是覆盖型描述符, 
+# 覆盖型描述符还可以分为两类，有__get__方法的覆盖型和没有__get__方法的覆盖型。
+#
+#Python学习——面向对象高级之描述符  https://blog.csdn.net/zhaoyun_zzz/article/details/82179481
+#python关于描述符__set__ ,__get__ , __delete__  https://blog.csdn.net/Mr_Slower/article/details/83590823
+#Python语言学习讲解十六：python之描述符__set__和__get__ 等解释  https://blog.csdn.net/Windgs_YF/article/details/53409482
+#
+#深入理解python super     https://coordinate.blog.csdn.net/article/details/79531891
 #
 
 import sys
@@ -22,6 +44,7 @@ notb=lambda s:'_'!=s[0]
 #list(filter(notb, dir(A)) )
 def xdir(cls):return [i for i in dir(cls) if not i.startswith('__')]
 
+
 class Foo():
 	def foo():print ('call foo');
 	def foo2(self):print ('call foo2');
@@ -34,8 +57,10 @@ class Mydesc():
 
 
 class G():
-	def __getattribute__(self, attr):print('geta')
-
+	ss=Mydesc()
+	def __getattribute__(self, name):print('geta-owner'); return super().__getattribute__(name)
+	#def __getattr__(self, name): pass
+g=G()
 
 
 ##
