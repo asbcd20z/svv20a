96abfb6a2df8782ff672fb654e434d17f0e5d19d
jqasbcd20z <jqasbcd20z@q.com>
2020-12-27 14:09:25 +0000

    dataclass.py--add

---
+++ tx19/python/dataclass.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+##-*- coding:utf-8 -*-
+
+import sys; sys.path ;print(sys.path)
+#import ctypesstructure as myt
+#pyy -ic'import ctypesstructure as myt'
+#pyy -i ctypesstructure.py
+#bytes.hex()
+
+
+'''memo
+Documentation » Python 标准库 » Python运行时服务 »
+ dataclasses --- 数据类  https://docs.python.org/zh-cn/3.9/library/dataclasses.html
+__annotations__  
+annotation  https://docs.python.org/3.9/glossary.html#term-annotation
+See variable annotation, function annotation, PEP 484 and PEP 526,  
+PEP 526 -- Syntax for Variable Annotations  https://www.python.org/dev/peps/pep-0526/
+PEP 484 -- Type Hints                       https://www.python.org/dev/peps/pep-0484/
+PEP 557 -- Data Classes                     https://www.python.org/dev/peps/pep-0557/
+mixin??
+#
+理解Python数据类（上）  https://www.sohu.com/a/246836180_114877  https://www.sohu.com/a/246836706_114877
+Python 3.7新功能之dataclass装饰器详解  https://www.jb51.net/article/138596.htm
+dataclasses-json  https://github.com/gosuai/dataclasses-json
+#
+'''
+
+#from dataclasses import dataclass
+#@dataclasses.dataclass(*, init=True, repr=True, eq=True, order=False, unsafe_hash=False, frozen=False)
+
+
+__debug__ 
+#import this
+#count: int = 0
+def foo(x:int, y:float): pass #function-__annotations__
+print(foo, foo.__annotations__, foo.__annotations__.__class__.__name__)
+
+
