97a8f6d7452eeb7883337a581496bd883a5e5b63
jqasbcd20z <jqasbcd20z@q.com>
2021-01-29 07:28:43 +0000

    test_pdb.py--inspect.signature

--- tx19/pytest/test_pdb.py
+++ tx19/pytest/test_pdb.py
@@ -2,8 +2,20 @@
 #pytest test_pdb.py
 
 import pdb
+import inspect  ## inspect.getfullargspec(func)
+from inspect import signature
+from inspect import formatargspec, getfullargspec
 
 #pdb.set_trace()
+
+def foo(a, *, b:int, **kwargs):pass
+sig = signature(foo)
+str(sig), sig.parameters
+str(sig.parameters['b'])
+##
+def f(a: int, b: float):pass
+inspect.formatargspec(*getfullargspec(f))
+
 
 def test_Hi2():
     print('hi1..')
