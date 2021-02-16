f33070ec60a5d9b6f76acd5a759457fab92946dd
jqasbcd20z <jqasbcd20z@q.com>
2021-02-16 12:58:24 +0800

    socketudp.py--sendto

--- tx19/python/socketudp.py
+++ tx19/python/socketudp.py
@@ -56,9 +56,25 @@ def doclient():
 ##
 
 ##
+def doclient2():
+    import socket
+    BUFSIZE = 1024
+    client = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    while True:
+        msg = input("2>> ").strip()
+        ip_port = ('127.0.0.1', 9999)
+        #print(msg.encode('utf-8'))
+        ret=client.sendto(msg.encode('utf-8'),ip_port)
+        print((ret, msg.encode('utf-8')))
+        #data,server_addr = client.recvfrom(BUFSIZE)
+        #print('c.recvfrom ',data,server_addr)
+    client.close()
+
+##
 def main(argv):
     if argv[1][0]=='s': doserver()
     if argv[1][0]=='c': doclient()
+    if argv[1][0:2]=='2c': doclient2()
 
 if __name__ == '__main__':
     import sys;print(sys.argv)
